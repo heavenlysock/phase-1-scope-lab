c1fd413877e3785187658eb9662e38e72c30e384
bobertsworld <bobertsworld@gmail.com>
2022-11-25 19:12:19 -0800

    commit tree 24f6ebf1b0012e09675aa31d721898958d4d09b7

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+
+
+}
+function setBestCustomer (){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// the only thing is if you interpolate and need to use backticks aka ``
+// }
+
+// function setBestCustomer() {
+//     bestCustomer = "not bob";
+
+// }
+    
+//there is a difference between what a function does and what a function returns
+
+
+
+
+// function upperCaseCustomerName() {
+
+//     let a = 5
+//     let b = 8
+//     let c = a + b
+
+//     return customerName.toUpperCase();
+
+
+// }
+
+// let number = upperCaseCustomerName()
+// you care about return function because you want to know what you can get out of it but there are things
+// that the function can do that has nothing to do with the IDBCursorWithValue.
+
+
+const leastFavoriteCustomer = 'JC'
+function changeLeastFavoriteCustomer(){
+    return LeastFavoriteCustomer = 'Matt'
+    
+    
+}
